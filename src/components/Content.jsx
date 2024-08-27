f839dabc3eaf60e14418a38cd2b706536e48985b
Timimah <timimah0117@gmail.com>
2024-08-27 19:55:10 +0100

    feat: add transition for page routing

---
+++ src/components/Content.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { AppRoutes } from "../routes/AppRoutes";
+
+export const Content = () => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage("fadeOut");
+  }, [location, displayLocation]);
+
+  return (
+    <div
+      className={transitionStage}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setDisplayLocation(location);
+          setTransistionStage("fadeIn");
+        }
+      }}
+    >
+      <AppRoutes displayLocation={displayLocation} />
+    </div>
+  );
+};
