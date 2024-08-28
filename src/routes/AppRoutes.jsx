19a42b09abc2561534a872ab2467bce1b8ab642e
Timimah <timimah0117@gmail.com>
2024-08-28 16:51:39 +0100

    chore: update routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Landing } from "../pages/website/Landing";
 
 export const AppRoutes = ({displayLocation}) => {
   return (
     <>
       <Routes location={displayLocation}>
         <Route path="/" />
-        <Route path="/create" />
+        <Route path="/landing" element={<Landing />} />
         <Route path="/tasks" />
       </Routes>
     </>
