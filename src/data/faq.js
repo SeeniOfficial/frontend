c2a09f30a457426e490f1b5ddf63501841eac70f
Timimah <timimah0117@gmail.com>
2024-08-28 16:55:38 +0100

    chore: faq data

---
+++ src/data/faq.js
@@ -0,0 +1,5 @@
+export const faqs = [
+    { question: 'Question 1?', answer: 'Answer to question 1.' },
+    { question: 'Question 2?', answer: 'Answer to question 2.' },
+    { question: 'Question 3?', answer: 'Answer to question 3.' },
+  ];
