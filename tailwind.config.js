4f9b1dc1c2cf71bd10ac08fdec778912fe0f82b2
Timimah <timimah0117@gmail.com>
2024-08-28 13:23:19 +0100

    build: update dependencies (github-pages)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-import { useState } from "react";
-const ThemeMode = () => {
-  const [theme, setTheme] = useState("light");
-};
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0084A7",
+        secondary: "#007C66",
+        neutral: "#FFA17A",
+        success: "#00D600",
+        error: "#FF0000",
+        whyte: "#F2F2F2",
+      },
+    },
   },
   plugins: [],
 };
