9cbce35c70746d556f0fb976d0aa7d292e3c4ecd
Timimah <timimah0117@gmail.com>
2024-08-30 00:00:11 +0100

    feat: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Content } from "./components/Content";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
+// import { Content } from "./components/Content";
+import { AppRoutes } from "./routes/AppRoutes";
 
 function App() {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage("fadeOut");
+  }, [location, displayLocation]);
+
   return (
-    <div className="w-full text-center">
-    <BrowserRouter>
-    <Content />
-    </BrowserRouter>
+    <div
+      className={transitionStage}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setDisplayLocation(location);
+          setTransistionStage("fadeIn");
+        }
+      }}
+    >
+      <AppRoutes displayLocation={displayLocation} />
     </div>
-  );
+    )
 }
 
 export default App;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { Landing } from "../pages/website/Landing";
 
-export const AppRoutes = ({displayLocation}) => {
+export const AppRoutes = ({ displayLocation }) => {
   return (
     <>
-      <Routes location={displayLocation}>
-        <Route path="/" />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/tasks" />
-      </Routes>
+        <Routes location={displayLocation}>
+          <Route path="/" element={<Landing />} />
+          {/* <Route path="/landing" />
+        <Route path="/tasks" /> */}
+        </Routes>
     </>
   );
 };
