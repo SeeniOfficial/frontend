b726c362955a0270077730231a17612acd5f91c8
Timimah <timimah0117@gmail.com>
2024-08-30 14:52:28 +0100

    build: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module. exports = {
     extend: {
       colors: {
         primary: "#0084A7",
+        drkprimary: "#000605",
         secondary: "#007C66",
         neutral: "#FFA17A",
         success: "#00D600",
