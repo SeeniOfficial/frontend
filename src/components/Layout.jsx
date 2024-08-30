06122da2c16401c6bc6e025b56a4989273561e1a
Timimah <timimah0117@gmail.com>
2024-08-30 13:34:14 +0100

    feat: create landing, layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Layout = ({children}) => {
   return (
-    <div className="p-4 max-w-xl mx-auto h-full">
+    <div className="mx-auto h-full">
         {children}
     </div>
   )
--- src/pages/website/Landing.jsx
+++ src/pages/website/Landing.jsx
@@ -5,24 +5,22 @@ import { ContentSection } from '../../components/ContentSection';
 import { FAQ } from '../../components/FAQ';
 import { Newsletter } from '../../components/Newsletter';
 import { Footer } from '../../components/Footer';
+import { Layout } from '../../components/Layout';
+import { Hero } from '../../components/Hero';
 
 export const Landing = () => {
   return (
-    <div className="bg-whyte dark:bg-black flex flex-col gap-0.5 w-full inset-0">
+    <Layout>
+      <div className="bg-whyte dark:bg-black flex flex-col gap-0.5 w-full inset-0">
       <Header/>
-      {/* <hr className="border-gray-200" /> */}
-      <ContentSection hero={true} />
-      {/* <hr className="border-gray-200" /> */}
+      <Hero />
       <Features />
-      {/* <hr className="border-gray-200" /> */}
       <ContentSection hero={false} />
-      {/* <hr className="border-gray-200" /> */}
       <ContentSection hero={false} reversed />
-      {/* <hr className="border-gray-200" /> */}
       <FAQ />
-      {/* <hr className="border-gray-200" /> */}
       <Newsletter />
       <Footer />
     </div>
+    </Layout>
   )
 }
