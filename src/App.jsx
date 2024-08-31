1baf33537c76b6c0db6cb690c8842846b5c658d4
Timimah <timimah0117@gmail.com>
2024-08-31 17:34:54 +0100

    feat: remove router animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, useLocation } from "react-router-dom";
+// import { BrowserRouter, useLocation } from "react-router-dom";
 // import { Content } from "./components/Content";
 import { AppRoutes } from "./routes/AppRoutes";
 
 function App() {
-  const location = useLocation();
-  const [displayLocation, setDisplayLocation] = useState(location);
-  const [transitionStage, setTransistionStage] = useState("fadeIn");
-
-  useEffect(() => {
-    if (location !== displayLocation) setTransistionStage("fadeOut");
-  }, [location, displayLocation]);
 
   return (
-    <div
-      className
-    >
+    <div>
       <AppRoutes />
     </div>
     )
