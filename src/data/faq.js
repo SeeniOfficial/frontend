3f83b2be446dd827cbeae0ccd47892f7f7d918b5
Timimah <timimah0117@gmail.com>
2024-09-02 20:35:28 +0100

    chore: FAQ data

--- src/data/faq.js
+++ src/data/faq.js
@@ -1,5 +1,7 @@
 export const faqs = [
-    { question: 'Question 1?', answer: 'Answer to question 1.' },
-    { question: 'Question 2?', answer: 'Answer to question 2.' },
-    { question: 'Question 3?', answer: 'Answer to question 3.' },
+    { question: 'What is Seeni?', answer: 'Answer to question 1.' },
+    { question: 'Why should i use Seeni?', answer: 'Answer to question 2.' },
+    { question: 'Can local vendors use Seeni?', answer: 'Answer to question 3.' },
+    { question: 'How can Seeni grow my sales?', answer: 'Answer to question 4.' },
+    { question: 'Can I pay for goods and services on Seeni?', answer: 'Answer to question 3.' },
   ];
