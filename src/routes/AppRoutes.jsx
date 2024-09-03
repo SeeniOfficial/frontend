f5561f9c678349da92f9136a8940273278b06279
Timimah <timimah0117@gmail.com>
2024-09-03 21:12:39 +0100

    chore: update routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { Landing } from "../pages/website/Landing";
+import { ProfilePage } from "../pages/app/ProfilePage";
+import { SignIn } from "../pages/website/SignIn";
+import { SignUp } from "../pages/website/SignUp";
+import { ForgotPassword } from "../pages/website/ForgotPassword";
 
 export const AppRoutes = () => {
   return (
     <>
+      <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
-          {/* <Route path="/landing" />
-        <Route path="/tasks" /> */}
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
+      </Router>
     </>
   );
 };
