cb81a537585ed2ef2c98f0bd510902e2a29ea069
Timimah <timimah0117@gmail.com>
2024-09-04 18:38:11 +0100

    chore: update app routes(protected routes)

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
 import { Landing } from "../pages/website/Landing";
 import { ProfilePage } from "../pages/app/ProfilePage";
 import { SignIn } from "../pages/website/SignIn";
 import { SignUp } from "../pages/website/SignUp";
 import { ForgotPassword } from "../pages/website/ForgotPassword";
+import { ProtectedRoute } from "./ProtectedRoutes";
 
 export const AppRoutes = () => {
   return (
@@ -15,7 +16,41 @@ export const AppRoutes = () => {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<ProfilePage />} />
+            {/* <Route
+              path="/app/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            /> */}
+            <Route
+              path="/app/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
+            {/* <Route
+              path="/app/catalog"
+              element={
+                <ProtectedRoute>
+                  <Catalog />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/app/messages"
+              element={
+                <ProtectedRoute>
+                  <Messages />
+                </ProtectedRoute>
+              }
+            /> */}
+
+            {/* Catch-all redirect to home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </>
