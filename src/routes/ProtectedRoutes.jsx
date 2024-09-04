b5cca0b6035e29d94d25eb4469f53d616ee70046
Timimah <timimah0117@gmail.com>
2024-09-04 18:38:05 +0100

    chore: create protected routes

---
+++ src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { AppLayout } from "../components/AppLayout";
+import { useAuthStore } from "../store/authStore";
+
+export const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, checkAuth } = useAuthStore();
+
+  // useEffect(() => {
+  //   checkAuth();
+  // }, [checkAuth]);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return (
+    <AppLayout>
+      <main className="flex-grow bg-whyte">{children}</main>
+    </AppLayout>
+  );
+};
