43c10b985e4d4bb340fb4cca23d918f2fff5bb21
Timimah <timimah0117@gmail.com>
2024-09-13 20:55:22 +0100

    chore: verifying email

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,7 @@ import { api } from './api';
 export const authService = {
   signIn: (credentials) => api.post('/api/auth/login', credentials),
   signUp: (userData) => api.post('/api/auth/register', userData),
-  verifyEmail: (token) =>  api.get(`/api/auth/verify-email/${token}`),
+  verifyEmail: (token) =>  api.get(`/api/auth/verify-email/:token`, { token }),
   isAuthenticated() {
     // Check if a valid token exists in localStorage
 const token = localStorage.getItem('authToken');
